053fb4c541fa5e3c08430190bd716a4a86c28ae5
pyloolex <alekslootsky@gmail.com>
2023-08-05 13:19:52 +0200

    Compose 2

--- app/src/main/java/com/volganl64/robotjump/MainActivity.kt
+++ app/src/main/java/com/volganl64/robotjump/MainActivity.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.draw.clip
 import com.volganl64.robotjump.ui.theme.RobotJumpTheme
 
 
@@ -28,25 +40,45 @@ class MainActivity : ComponentActivity() {
         //     }
         // }
         setContent {
-            MessageCard("motherfucker")
+            MessageCard(Message("Bill", "Hello, what are you up to?"))
         }
     }
 }
 
 
+data class Message(val author: String, val body: String)
+
+
 @Composable
-fun MessageCard(name: String)
+fun MessageCard(msg: Message)
 {
-    Text(text="Hello $name!")
+    Row(modifier=Modifier.padding(all=8.dp)) {
+        Image(
+            painter=painterResource(R.drawable.robotimage),
+            contentDescription="Robot",
+            modifier=Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier=Modifier.width(8.dp))
+
+        Column {
+            Text(text=msg.author)
+            Spacer(modifier=Modifier.height(4.dp))
+            Text(text=msg.body)
+        }
+    }
 }
 
 
 @Preview
 @Composable
-fun PreviewMessageCard()
+fun MessageExample()
 {
-    MessageCard("Android")
+    MessageCard(Message("Jack", "Running errands."))
 }
+
 
 // @Composable
 // fun Greeting(name: String, modifier: Modifier = Modifier) {
