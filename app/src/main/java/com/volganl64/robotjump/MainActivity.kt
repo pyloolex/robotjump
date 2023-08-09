511a22c1b3e24aef6c1449ae8c4bf2b25310da0e
pyloolex <alekslootsky@gmail.com>
2023-08-09 18:39:09 +0200

    Compose tutorial done

--- app/src/main/java/com/volganl64/robotjump/MainActivity.kt
+++ app/src/main/java/com/volganl64/robotjump/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.content.res.Configuration
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,17 +17,98 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.draw.clip
 import com.volganl64.robotjump.ui.theme.RobotJumpTheme
+
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Lexi",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Lexi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Lexi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Lexi",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Lexi",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Lexi",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Lexi",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Lexi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Lexi",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Lexi",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Lexi",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Lexi",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
 
 
 class MainActivity : ComponentActivity() {
@@ -41,10 +125,11 @@ class MainActivity : ComponentActivity() {
         //         }
         //     }
         // }
+
         setContent {
             RobotJumpTheme {
                 Surface(modifier=Modifier.fillMaxSize()) {
-                    MessageCard(Message("Bill", "Hello, what are you up to?"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -70,7 +155,13 @@ fun MessageCard(msg: Message)
 
         Spacer(modifier=Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary
+            else MaterialTheme.colorScheme.surface
+        )
+
+        Column(modifier=Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text=msg.author,
                 color=MaterialTheme.colorScheme.secondary,
@@ -78,15 +169,31 @@ fun MessageCard(msg: Message)
             )
             Spacer(modifier=Modifier.height(4.dp))
             Surface(shape=MaterialTheme.shapes.medium,
-                    shadowElevation=1.dp) {
+                    shadowElevation=1.dp,
+                    color=surfaceColor,
+                    modifier=Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text=msg.body,
                     modifier=Modifier.padding(all=4.dp),
                     style=MaterialTheme.typography.bodyMedium,
+                    maxLines=if (isExpanded) Int.MAX_VALUE else 1,
                 )
             }
         }
     }
+}
+
+
+@Composable
+fun Conversation(messages: List<Message>)
+{
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+
 }
 
 
@@ -99,7 +206,7 @@ fun MessageExample()
 {
     RobotJumpTheme {
         Surface {
-            MessageCard(Message("Jack", "Running errands."))
+            Conversation(SampleData.conversationSample)
         }
     }
 }
