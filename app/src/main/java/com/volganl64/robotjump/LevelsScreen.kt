c631ca7efe531a6b0d05476717369651ec4e8206
pyloolex <alekslootsky@gmail.com>
2023-09-10 20:57:52 +0200

    References to levels work

--- app/src/main/java/com/volganl64/robotjump/LevelsScreen.kt
+++ app/src/main/java/com/volganl64/robotjump/LevelsScreen.kt
@@ -6,14 +6,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavController
 
 
@@ -21,12 +27,27 @@ import androidx.navigation.NavController
 fun LevelsScreen(navigation: NavController)
 {
     Box(Modifier.background(Color.Blue).fillMaxSize()) {
-        Button(
-                { navigation.navigate("game/2") },
-                Modifier.padding(DEFAULT_MARGIN).height(30.dp).width(30.dp),
-                shape=RoundedCornerShape(5),
-                colors=ButtonDefaults.buttonColors(containerColor=BUTTON_COLOR),
-                contentPadding=PaddingValues(0.dp),
-        ) {}
+        LazyColumn {
+            itemsIndexed(Levels) { i, level ->
+                Button(
+                    { navigation.navigate("game/$i") },
+                    Modifier.padding(DEFAULT_MARGIN).height(70.dp).width(70.dp),
+                    shape=RoundedCornerShape(5),
+                    colors=ButtonDefaults.buttonColors(containerColor=BUTTON_COLOR),
+                    contentPadding=PaddingValues(0.dp),
+                ) {
+                    Text("${i + 1} (0)")
+                }
+            }
+        }
     }
+}
+
+
+@Preview
+@Composable
+fun LevelsPreview()
+{
+    val navController = rememberNavController()
+    LevelsScreen(navController)
 }
